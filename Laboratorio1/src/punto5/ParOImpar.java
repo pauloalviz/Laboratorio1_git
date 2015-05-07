fb60a0ebd67b2d35aa099fd4edac0bd256fb51cd
paulo <paulo@loyal3.com>
2015-05-07 15:38:39 -0500

    lab 5

---
+++ Laboratorio1/src/punto5/ParOImpar.java
@@ -0,0 +1,20 @@
+package punto5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ParOImpar {
+	public static void main(String[] args) throws IOException { 
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.println("Ingresa un numero entero: ");  
+        int numero = Integer.parseInt(in.readLine( )); 
+        //con el numero ingresado se calcula a traves del modulo si es par o impar
+        if(numero%2==0){  
+             System.out.println(numero+" es par");  
+        }else{  
+            System.out.println(numero+" es impar");  
+        }        
+     }  
+}
